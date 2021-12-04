f7bf9052fe5717d30812adb3e7da57cb6a33f4ef
Hien Le <ltdhien@hotmail.com>
2021-12-05 04:38:11 +0700

    setup nodemon

--- src/server.js
+++ src/server.js
@@ -6,3 +6,7 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+let port =  process.env.PORT ||  1772;
+app.listen(port, () => {
+    console.log("App runing at the port:" + port);
+});
